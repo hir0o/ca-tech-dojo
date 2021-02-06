f857124a985d74e94b2e230fd3b69fbc15c3220e
mkmk4423 <mk.9.q.kyu@gmail.com>
2021-02-06 10:38:27 +0900

    Add: 簡単なリクエスト処理を作成

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"ca-tech-dojo/controller"
 
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.POST("/user/create", controller.UserCreate)
+	e.GET("/user/get", controller.UserGet)
 }
