889b3cab52b9f1d49d28ed1303c5ac960b4b1fcc
mkmk4423 <mk.9.q.kyu@gmail.com>
2021-02-06 11:07:03 +0900

    Add: userを作成する関数

---
+++ record/user.go
@@ -0,0 +1,28 @@
+package record
+
+import (
+	"ca-tech-dojo/db"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// データ型
+type User struct {
+	ID    int64
+	Name  string
+	Token string
+}
+
+// データの作成
+func CreateUser(name string) string {
+	db := db.Connect()
+	token := "123456789" // TODO: tokenを生成する
+	const sql = "INSERT INTO user(name token) values (? ?)"
+	_, err := db.Exec(sql, name, token)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	return token
+}
